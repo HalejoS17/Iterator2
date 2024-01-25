6cde6e24e12d512798245c3a05c455e5bede1503
ASUS <ASUS@SHA>
2024-01-25 10:00:15 -0500

    Agregar archivos de proyecto.

---
+++ ConcreteAggregate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorPattern
+{
+    public class ConcreteAggregate<T> : Aggregate<T>
+    {
+        private List<T> _collection;
+
+        public ConcreteAggregate()
+        {
+            _collection = new List<T>();
+        }
+
+        public void AddItem(T item)
+        {
+            _collection.Add(item);
+        }
+
+        public Iterator<T> CreateIterator()
+        {
+            return new ConcreteIterator<T>(_collection);
+        }
+    }
+
+}
---
+++ ConcreteIterator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorPattern
+{
+    public class ConcreteIterator<T> : Iterator<T>
+    {
+        private List<T> _collection;
+        private int _index;
+
+        public ConcreteIterator(List<T> collection)
+        {
+            _collection = collection;
+            _index = 0;
+        }
+
+        public bool HasNext()
+        {
+            return _index < _collection.Count;
+        }
+
+        public T Next()
+        {
+            T item = _collection[_index];
+            _index++;
+            return item;
+        }
+    }
+
+}
